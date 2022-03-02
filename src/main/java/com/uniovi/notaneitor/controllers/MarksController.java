53378cb3b79d2d67646b440e93b196e86c894cbb
Alejandro Álvarez Solís <UO232627@uniovi.es>
2022-03-02 13:33:59 +0100

    SDI-2122-301-4.4-SpringBoot-Consulta

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -1,6 +1,7 @@
 package com.uniovi.notaneitor.controllers;
 
 import com.uniovi.notaneitor.entities.Mark;
+import com.uniovi.notaneitor.entities.User;
 import com.uniovi.notaneitor.services.MarksService;
 import com.uniovi.notaneitor.services.UsersService;
 import com.uniovi.notaneitor.validators.MarksFormValidator;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 public class MarksController {
@@ -29,8 +31,10 @@ public class MarksController {
     private HttpSession httpSession;
 
     @RequestMapping("/mark/list")
-    public String getList(Model model){
-        model.addAttribute("markList", marksService.getMarks());
+    public String getList(Model model, Principal principal){
+        String dni = principal.getName();
+        User user = usersService.getUserByDni(dni);
+        model.addAttribute("markList", marksService.getMarksForUser(user));
         return "/mark/list";
     }
 
@@ -63,8 +67,10 @@ public class MarksController {
     }
 
     @RequestMapping("/mark/list/update")
-    public String updateList(Model model){
-        model.addAttribute("markList", marksService.getMarks());
+    public String updateList(Model model, Principal principal){
+        String dni = principal.getName();
+        User user = usersService.getUserByDni(dni);
+        model.addAttribute("markList", marksService.getMarksForUser(user));
         return "/mark/list::tableMarks";
     }
 
