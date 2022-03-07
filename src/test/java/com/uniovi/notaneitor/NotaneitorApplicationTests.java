9e48072e9507dd45b4203535bfbe16abaa784b6c
Alejandro Álvarez Solís <UO232627@uniovi.es>
2022-03-07 20:07:10 +0100

    SDI-2122-301-5.4-Casos Uso VistaHome.

---
+++ src/test/java/com/uniovi/notaneitor/NotaneitorApplicationTests.java
@@ -0,0 +1,87 @@
+package com.uniovi.notaneitor;
+
+import com.uniovi.notaneitor.pageobjects.PO_HomeView;
+import com.uniovi.notaneitor.pageobjects.PO_Properties;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class NotaneitorApplicationTests {
+
+    static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    static String Geckodriver = "D:\\Universidad\\Sistemas Distribuidos e Internet\\Practicas\\Sesion 6\\sesion06\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+
+    // Común a Windows y a MACOSX
+    static WebDriver driver = getDriver(PathFirefox, Geckodriver);
+    static String URL = "http://localhost:8090";
+
+    public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
+        System.setProperty("webdriver.firefox.bin", PathFirefox);
+        System.setProperty("webdriver.gecko.driver", Geckodriver);
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    @BeforeEach
+    public void setUp(){
+        driver.navigate().to(URL);
+    }
+
+    //Después de cada prueba se borran las cookies del navegador
+    @AfterEach
+    public void tearDown(){
+        driver.manage().deleteAllCookies();
+    }
+
+    //Antes de la primera prueba
+    @BeforeAll
+    static public void begin() {}
+
+    //Al finalizar la última prueba
+    @AfterAll
+    static public void end() {
+        //Cerramos el navegador al finalizar las pruebas
+        driver.quit();
+    }
+
+    @Test
+    @Order(1)
+    void PR01A(){
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+    }
+
+    @Test
+    @Order(2)
+    void PR01B(){
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver, PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(), PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+    }
+
+    //PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
+    @Test
+    @Order(3)
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
+
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
+    @Test
+    @Order(4)
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
+    @Test
+    @Order(5)
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+    }
+
+}
